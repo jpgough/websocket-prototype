b11c860bcd7717dd71da175779af112cb1eca761
James Gough <jpgough@gmail.com>
2021-07-05 11:32:10 +0100

    Bug Fix configuration on small file

--- src/main/java/com/jpgough/websocket/server/WebSocketConfig.java
+++ src/main/java/com/jpgough/websocket/server/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.jpgough.websocket.server;
 
 import com.jpgough.websocket.server.handlers.LargeFileWebSocketHandler;
 import com.jpgough.websocket.server.handlers.SimpleAckWebSocketHandler;
+import com.jpgough.websocket.server.handlers.SmallFileWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.*;
 
@@ -12,7 +13,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         webSocketHandlerRegistry.addHandler(new LargeFileWebSocketHandler(), "/100mb");
-        webSocketHandlerRegistry.addHandler(new LargeFileWebSocketHandler(), "/1mb");
+        webSocketHandlerRegistry.addHandler(new SmallFileWebSocketHandler(), "/1mb");
         webSocketHandlerRegistry.addHandler(new SimpleAckWebSocketHandler(), "/ack");
     }
 }
